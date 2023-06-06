7c01e6f1bccd45886e0b714be06203c07124c097
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-06-06 10:21:14 -0600

    Add new files: paperScissorsStone html and js

---
+++ paperScissorsStone.js
@@ -0,0 +1,11 @@
+//Create a function to get computer choice (paper, scissors or stone)
+    //create a random result using mathnumber?
+
+
+
+//create an input for the player
+    //correct for case sensitve issues
+
+//Compare the two and declare a winner/loser or tie 
+
+//create a game loop function that plays five rounds and records the winner
