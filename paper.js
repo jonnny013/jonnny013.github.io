8c37564a4b56d23ee88306ced2b72267937a180f
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-06-06 19:54:17 -0600

    Save switch file

--- paper.js
+++ paper.js
@@ -1,15 +1,20 @@
 //player input
+function playerInput(){
 
+}
 
 //computer input
+function computerInput(){
+
+}
 
 //create a play function
     //it will take the computer input and player input and assess the winner
 function play(playerInput, computerInput) {
     let player = playerInput;
     let comp = computerInput;
-    let winner;
-    switch (player, comp) {
+    let winner = 0;
+    switch (winner) {
         case (player === "rock" && comp === "scissors"):
         case (player === "paper" && comp === "rock"):
         case (player === "scissors" && comp === "paper"):
@@ -20,7 +25,7 @@ function play(playerInput, computerInput) {
         case (player === "scissors" && comp === "rock"):
         return "loser";
         break;
-        default:
+       default:
         return "tie";
     }
 
