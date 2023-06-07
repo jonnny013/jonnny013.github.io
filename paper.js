be3351db3f994f3886d523e5c0a941e16490a0b5
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-06-06 22:26:23 -0600

    Add finishing statements and for loop to paper.js

--- paper.js
+++ paper.js
@@ -31,7 +31,7 @@ function play(playerInput, computerInput) {
         if ((player === "rock" && comp === "scissors") ||
         (player === "paper" && comp === "rock") ||
         (player === "scissors" && comp === "paper")) {
-            console.log("winner");
+
         return "winner"; }
       
         else if ((player === "rock" && comp === "paper") ||
@@ -47,10 +47,10 @@ function play(playerInput, computerInput) {
 
 
 //create a winning output function
-function playRound () {
-    let player = playerInput();
+function playRound (x, y) {
+    let player = x;
 
-    let computer = computerInput();
+    let computer = y;
 
     let result = play(player, computer);
 
@@ -65,6 +65,33 @@ function playRound () {
     }
     else {return "What happened?"}
 }
-let finish = playRound();
-console.log(finish); 
+
 //create a loop to play five rounds
+function game() {
+    let userScore = 0;
+    let compScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let player = playerInput();
+        let computer = computerInput();
+        let finish = playRound(player, computer);
+
+
+        let score = play(player, computer);
+
+        if (score == "winner") {
+            userScore++;
+        }
+        else if (score == "loser") {
+            compScore++;
+        }
+        console.log(finish + ` Score = Computer ${compScore} and you ${userScore}`); 
+    }
+    console.log("Game over:");
+    if (compScore < userScore) {
+        console.log("You are the winner!");
+    }
+    else if (compScore > userScore) {
+        console.log("You lost...");
+    }
+}
+game();
