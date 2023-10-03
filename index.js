7b62d8af64cd2fd13f17335f323e0ae1200ad7f9
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-03 15:51:52 +0800

    Fix scrolling on main projects for project cards

---
+++ index.js
@@ -0,0 +1,9 @@
+const arrowScrollLeft = () => {
+    let left = document.querySelector(".scroll-divs")
+    left.scrollBy(350, 0)
+}   
+
+const arrowScrollRight = () => {
+  let right = document.querySelector(".scroll-divs");
+  right.scrollBy(-350, 0);
+};   
