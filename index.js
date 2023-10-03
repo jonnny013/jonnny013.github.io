4bf865dc5a089562364f75a061791cef989006a0
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-03 21:32:04 +0800

    Change a link from title to entire card

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ const arrowScrollRight = () => {
   let right = document.querySelector(".scroll-divs");
   right.scrollBy(300, 0);
 };   
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
